9c4414e2f56112d696f6c97117f1cc7923355bcc
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-29 14:39:30 +0700

    chore(models/user-permission): organizationPermissionId -> permissionName

--- src/models/user-permission.model.ts
+++ src/models/user-permission.model.ts
@@ -4,7 +4,7 @@ import { UserPermission } from '../apis/hts/common/common';
 export class UserPermissionModel extends Model implements UserPermission {
   id: number;
   userId: number;
-  organizationPermissionId: number;
+  permissionName: string;
 
   static tableName = 'user_permission';
 }
