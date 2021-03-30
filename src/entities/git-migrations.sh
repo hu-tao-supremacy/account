f90eeabbf2186360100d71afb0da476943cce778
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 10:35:58 +0700

    feat(entities): download all TypeORM entities from @hu-tao-supremacy/migrations

--- src/entities/git-migrations.sh
+++ src/entities/git-migrations.sh
@@ -1,1 +1,4 @@
-curl 'https://api.github.com/repos/hu-tao-supremacy/migrations/contents/entities' --silent | jq '.[]' | jq '.download_url'
+ENTITIES=$(curl 'https://api.github.com/repos/hu-tao-supremacy/migrations/contents/entities' --silent | jq '.[]' | jq '.download_url' -r)
+for e in $ENTITIES; do
+  wget $e;
+done
