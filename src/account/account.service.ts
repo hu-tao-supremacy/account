30ea5113e301e48b59482d2b2b21191e2b577312
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 11:45:13 +0700

    feat: use TypeORM

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,34 +1,39 @@
+import { Organization } from '@entities/organization.entity';
+import { UserOrganization } from '@entities/user-organization.entity';
+import { UserPermission } from '@entities/user-permission.entity';
+import { User } from '@entities/user.entity';
 import { AccessTokenPayload } from '@internal/account/service';
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
 import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Permission } from 'src/apis/hts/common/common';
-import { UserPermissionModel } from 'src/models/user-permission.model';
-import { UserModel } from 'src/models/user.model';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AccountService {
   constructor(
-    @Inject(UserModel) private readonly userModel: typeof UserModel,
-    @Inject(UserPermissionModel)
-    private readonly userPermissionModel: typeof UserPermissionModel,
+    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Organization) private organizationRepository: Repository<Organization>,
+    @InjectRepository(UserOrganization) private userOrganizationRepository: Repository<UserOrganization>,
+    @InjectRepository(UserPermission) private userPermission: Repository<UserPermission>,
     private readonly jwtService: JwtService,
   ) {}
 
-  async ping(): Promise<string> {
-    let query = await this.userModel.knex().raw('SELECT VERSION()');
-    return query.rows[0].version as string;
+  async ping(): boolean {
+    return true;
   }
 
-  async userHasPermissionInOrganization(
+  userHasPermissionInOrganization(
     userId: number,
     organizationId: number,
     permissionName: Permission,
-  ): Promise<boolean> {
-    let query = await this.userPermissionModel
-      .query()
-      .where({ userId, organizationId, permissionName });
-    return query.length === 1;
+  ): Observable<boolean> {
+    return from(this.userPermission.findAndCount({ where: { userId, organizationId, permissionName } })).pipe(
+      map((project) => project[1]),
+      map((count) => count === 1),
+    );
   }
 
   generateAccessToken(userId: number): string {
@@ -41,7 +46,7 @@ export class AccountService {
     });
   }
 
-  getUserByChulaId(id: number): Observable<UserModel> {
-    return from(this.userModel.query().findOne({ chulaId: id }));
+  getUserByChulaId(id: number): Observable<User> {
+    return from(this.userRepository.findOne({ where: { chulaId: id } }));
   }
 }
