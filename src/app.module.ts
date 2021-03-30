b56ff53d8e5b064cc193d2dee16c9d5340fb99ca
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 11:33:50 +0700

    feat(app): add TypeOrmModule()

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Organization } from '@entities/organization.entity';
+import { User } from '@entities/user.entity';
+import { UserOrganization } from '@entities/user-organization.entity';
+import { UserPermission } from '@entities/user-permission.entity';
 
 @Module({
-  imports: [AccountModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User, Organization, UserOrganization, UserPermission],
+      synchronize: false,
+    }),
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
