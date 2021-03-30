63f944d70cfa8e166d8b296f5da627eecb641a06
FleeingSunlight <gh@fleeingsunlight.dev>
2021-03-30 20:17:13 +0700

    feat(main.ts): make NestJS hybrid application

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,39 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.GRPC,
-      options: {
-        url: `0.0.0.0:${process.env.GRPC_PORT}`,
-        package: protobufPackage,
-        protoPath: join(
-          __dirname,
-          '../../apis/proto/hts/account/service.proto',
-        ),
-        loader: {
-          includeDirs: [join(__dirname, '../../apis/proto')],
-        },
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   {
+  //     transport: Transport.GRPC,
+  //     options: {
+  //       url: `0.0.0.0:${process.env.GRPC_PORT}`,
+  //       package: protobufPackage,
+  //       protoPath: join(
+  //         __dirname,
+  //         '../../apis/proto/hts/account/service.proto',
+  //       ),
+  //       loader: {
+  //         includeDirs: [join(__dirname, '../../apis/proto')],
+  //       },
+  //     },
+  //   },
+  // );
+  // app.listen(() => console.log('Microservice is listening'));
+
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      url: `0.0.0.0:${process.env.GRPC_PORT}`,
+      package: protobufPackage,
+      protoPath: join(__dirname, '../../apis/proto/hts/account/service.proto'),
+      loader: {
+        includeDirs: [join(__dirname, '../../apis/proto')],
       },
     },
-  );
-  app.listen(() => console.log('Microservice is listening'));
+  });
+
+  await app.startAllMicroservicesAsync();
+  await app.listen(5001);
 }
 bootstrap();
