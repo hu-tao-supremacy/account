c82c0e16bc953622c397bab361344ae855c761a1
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-10 20:21:39 +0700

    feat(entities): update

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { PrimaryGeneratedColumn, Column, Entity, Index } from 'typeorm';
+import { PrimaryGeneratedColumn, Column, Entity, Index } from "typeorm";
 
 @Entity()
 export class User {
@@ -17,12 +17,24 @@ export class User {
   @Column({ nullable: true })
   nickname?: string;
 
-  @Index({ unique: true, where: 'chula_id IS NOT NULL' })
+  @Index({ unique: true, where: "chula_id IS NOT NULL" })
   @Column({ nullable: true })
   chulaId?: string;
 
   @Column({ nullable: true })
   address?: string;
+
+  @Column({ nullable: true })
+  phoneNumber?: string;
+
+  @Column({ nullable: true })
+  district?: string;
+
+  @Column({ nullable: true })
+  province?: string;
+
+  @Column({ nullable: true })
+  zipCode?: string;
 
   @Column({ nullable: true })
   profilePictureUrl?: string;
@@ -33,6 +45,6 @@ export class User {
   @Column({ default: false })
   didSetup: boolean = false;
 
-  @Column('enum', { enum: ['M', 'F', 'NS'] })
+  @Column("enum", { enum: ['__RESERVED__', "M", "F", "NS"] })
   gender: string;
 }
