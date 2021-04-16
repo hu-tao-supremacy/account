9eec915d47a126f501167056b1a28efab843a500
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-16 23:30:24 +0700

    feat(adapters): add super() to OrganizationAdapter and UserEventAdapter

--- src/adapters/organization.adapter.ts
+++ src/adapters/organization.adapter.ts
@@ -2,4 +2,8 @@ import { Organization as Entity } from '@entities/organization.entity';
 import { Organization as API } from '@api/common/common';
 import { BaseAdapter } from './base.adapter';
 
-export class OrganizationAdapter extends BaseAdapter<API, Entity> {}
+export class OrganizationAdapter extends BaseAdapter<API, Entity> {
+  constructor() {
+    super(API);
+  }
+}
--- src/adapters/user-event.adapter.ts
+++ src/adapters/user-event.adapter.ts
@@ -2,4 +2,8 @@ import { BaseAdapter } from './base.adapter';
 import { UserEvent as API } from '@api/common/common';
 import { UserEvent as Entity } from '@entities/user-event.entity';
 
-export class UserEventAdapter extends BaseAdapter<API, Entity> {}
+export class UserEventAdapter extends BaseAdapter<API, Entity> {
+  constructor() {
+    super(API);
+  }
+}
