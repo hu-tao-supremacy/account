ea4b4fc5d1f439b9f5daa47912956dfd1f8dee20
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-17 21:27:28 +0700

    fix(service): firstName -> first_name

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -238,7 +238,7 @@ export class AccountService {
   searchUser(keyword: string): Promise<User[]> {
     return this.userRepository
       .createQueryBuilder('user')
-      .where('CONCAT(firstName, lastName, email) LIKE :keyword LIMIT 25', { keyword })
+      .where('CONCAT(email, chula_id, first_name, last_name) LIKE :keyword LIMIT 25', { keyword })
       .getMany();
   }
 
